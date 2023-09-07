5c06ddf3abd7e523355aac6bb0c3a9058da6c7df
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-07 07:26:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - starting point for the execution of the program
- *
- * Description: print the line and returen 0 if it runs successfully
+ * main - the entry point
  *
  * return: the programm should return 0 
  */
