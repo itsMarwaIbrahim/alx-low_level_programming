c2bc4f9918f0aafb4df08793816ef56ad63d5e85
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-07 11:21:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,15 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	char* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	const char* str = "print this";
+
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	return (1);
 
