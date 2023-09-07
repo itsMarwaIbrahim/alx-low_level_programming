02ef95a1ec4d6132709adb8b1468659d0fb3204e
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-07 07:20:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,11 @@
  *
  * return: the programm should return 0 if it runs successfully
  */
-
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
+
 }
