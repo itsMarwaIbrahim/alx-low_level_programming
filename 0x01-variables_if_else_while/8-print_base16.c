6504a809f15100cb7a2dcad2d2a1cd5c26c7c866
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-08 12:41:57 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retrun: Always 0 (Success)
+ */
+int main(void)
+{
+	int r; char m;
+
+	for (r = 0; r < 10; r++)
+		putchar(r + '0');
+
+	for (m = 'a'; m <= 'f'; m++)
+		putchar(m);
+
+	putchar('\n');
+	return (0);
+}
