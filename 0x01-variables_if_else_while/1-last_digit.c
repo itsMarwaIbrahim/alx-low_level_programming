dc41d593f2eb070dd894fb65efef2b84a736ccb8
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-08 08:30:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	}
-	else if (x = 0)
+	else if (x == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
