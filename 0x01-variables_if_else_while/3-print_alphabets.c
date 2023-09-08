bdf56824e9ea499682c2a45bd966ae3c8c17f17a
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-08 10:02:22 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+	char upp;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+
+	for (upp = 'A'; upp <= 'Z'; upp++)
+	{
+		putchar(upp);
+	}
+
+	putchar('\n');
+	return (0);
+}
