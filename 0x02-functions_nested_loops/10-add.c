c785532f1954e03672003d71cbbea37e5c082510
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-12 15:34:34 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @x: first number, @y: second number
+ * Return: result z
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
