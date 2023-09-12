ac1bfa5073027872ef978372e546ceeeadbff41f
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-12 11:36:47 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,9 @@
  */
 void jack_bauer(void)
 {
-       	for (int hour = 0; hour < 24; hour++)
+       	for (hour = 0; hour < 24; hour++)
 	{
-		for (int minute = 0; minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 			_putchar(hour,minute);
 			_putchar('\n')
 	}
