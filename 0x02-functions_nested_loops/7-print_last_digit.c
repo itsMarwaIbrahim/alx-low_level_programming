a4fb4600ebf51ba85d0c5797b52e5e616ad375d6
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-12 11:13:11 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,12 @@ int print_last_digit(int w)
 	int lastd = w % 10;
 
 	if (lastd < 0)
-		_putchar (-lastd + '0');
+	{
+		_putchar(-lastd + '0');
 		return (-lastd);
-
+	}
 	else
-		return (lastd + '0');
+	{
+		_putchar(lastd + '0');
+		return (lastd);
 }
