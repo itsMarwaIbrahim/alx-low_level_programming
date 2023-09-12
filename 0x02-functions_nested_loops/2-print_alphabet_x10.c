7a80f882a35086f0dda0a39094892538af864a40
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-12 06:42:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,13 @@ void print_alphabet_x10(void)
 	int lines;
 	char letters;
 
-	for (lines = 0; lines <= 9; lines++)
+	for (lines = 0; lines < 10; lines++)
 	{
 		for (letters = 0; letters <= 9; letters++)
+		{
 			_putchar(letters);
+		}
+
 		_putchar('\n');
 	}
 }
