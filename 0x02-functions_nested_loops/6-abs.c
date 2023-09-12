80463720c403fae66f6c577587c014f1004253b7
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-12 10:01:16 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - check this
+ * 
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+
+	else if (r < 0)
+	{
+		return (-r);
+	}
+
+	else
+		return (r);
+}
