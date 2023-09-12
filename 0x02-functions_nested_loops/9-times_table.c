875fb994f68e2cabfd8c76e2142b26a0b3fe8842
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-12 13:03:00 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; x < 10; y++)
+		{
+			z = y * x;
+			_putchar(z);
+		}
+		_puchar('\n');
+	}
+}
