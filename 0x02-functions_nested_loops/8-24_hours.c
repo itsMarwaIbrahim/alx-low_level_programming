914cf43f6b575fcee2ed375028dd387328ccfdc2
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-12 12:16:35 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,13 +2,13 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * Return:nothing
+ * Return: nothing
  */
 void jack_bauer(void)
 {
 	int hour;
 	int minute;
-	
+
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
