20b28e4691adc59d1c3c34207443ad2625bf0137
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-15 15:52:22 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
-			printf("Fizz Buzz\n");
+			printf("FizzBuzz\n");
 		else if (x % 3 == 0)
 			printf("Fizz\n");
 		else if (x % 5 == 0)
 			printf("Buzz\n");
 		else
-			printf(" %d\n", x);
+			printf("%d\n", x);
 	}
 	return (0);
 }
