99a0d8251e2e14f9a70022ec6c89c6e354aff863
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-21 13:40:24 -0700

    n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return 0 if success
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+}
