fa50e78ddb9094e7fbdd3e0a17d5d36b304c868e
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-22 11:58:30 -0700

    7

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @
+ * Return:
+ */
+char *cap_string(char *)
+{
+
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @
+ * Return:
+ */
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+		i++;
+	}
+	return (str);
+}
