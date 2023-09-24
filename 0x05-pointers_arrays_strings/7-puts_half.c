eb503a9f52d3aa920533126f36cd6da0d80bcef6
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-24 15:41:52 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,15 +3,12 @@
 /**
  * puts_half -  prints half of a string
  * @str: string
- * @len: integer to count the length of the string
- * @mid: middle of the string
- * @j: variable
  * Return: void
  */
 void puts_half(char *str)
 {
 	int len = 0, mid, j;
-	
+
 	while (str[len] != '\0')
 		len++;
 	if (len % 2 == 0)
