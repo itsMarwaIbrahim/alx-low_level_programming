5a3f91335bf78d0320e8cbfad2b408bdf8d37dbb
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-25 06:38:54 -0700

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,11 +13,11 @@ char *_strstr(char *haystack, char *needle)
 	{
 		while (*needle != '\0' && *haystack == *needle)
 		{
-			if (*needle != '\0')
-				return (haystack);
 			haystack++;
 			needle++;
 		}
+		if (!*needle)
+			return (haystack);
 		haystack++;
 	}
 	return (NULL);
