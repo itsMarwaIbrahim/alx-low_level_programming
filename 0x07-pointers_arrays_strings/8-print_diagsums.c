af82bff1d29b7338ff746ac4aa26bf6c263c1308
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-25 09:17:03 -0700

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,4 +9,12 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i, first = 0, second = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		first = first + a[i + size * i];
+		second = second + a[(size - 1 - i) + size * i];
+	}
+	printf("%d, %d\n", first, second);
+}
