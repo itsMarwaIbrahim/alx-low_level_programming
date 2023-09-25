a1e989c2c0102151879f094699cab8f13fee17f3
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-25 07:48:20 -0700

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums -  prints the sum of the two diagonals
+ * @a: the pointer to the matrix
+ * @size: the size of the matrix
+ * Return:
+ */
+void print_diagsums(int *a, int size)
+{
+
