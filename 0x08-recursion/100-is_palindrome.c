aaf7c5259655c6938eae1576f3b0d298bad119da
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-28 12:58:09 -0700

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -21,7 +21,7 @@ int true_palindrome(char *s, int start, int end)
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not
  * @s: string
- * Return:
+ * Return: 1 or 0
  */
 int is_palindrome(char *s)
 {
