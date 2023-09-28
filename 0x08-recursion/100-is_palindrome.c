fb0c9908c21ac1b32a6c504091da3a4287149034
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-28 12:16:11 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int true_palindrome(char *s, int first, int last)
+{
+	if (first <= last)
+		return (1);
+	if (s[first] == s[last])
+		return (1);
+	return true_palindrome(s, first - 1, last + 1);
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: string
+ * Return:
+ */
+int is_palindrome(char *s)
+{
+	int length = 0;
+
+	if (*s != '\0')
+	{
+		length = length + _strlen_recursion(s + 1);
+		length++;
+	}
+	if (length == 0 || length == 1)
+		return (1);
+	return true_palindrome(s, 0, length - 1);
+}
