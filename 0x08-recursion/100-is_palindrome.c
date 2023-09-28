d32eea6e7919e37c68b5214fa867656429380c0e
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-28 12:41:10 -0700

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,7 @@ int true_palindrome(char *s, int start, int end)
 		return (0);
 	if (start <= end)
 		return (1);
-	return true_palindrome(s, end - 1, start + 1);
+	return true_palindrome(s, start + 1, end - 1);
 }
 
 /**
