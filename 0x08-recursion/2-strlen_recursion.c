8988529181043b40180b9afc9da59b3edd85496a
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-09-28 04:24:21 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return:
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_strlen_recursion(s + 1);
+		count++;
+	}
+	return count;
+}
