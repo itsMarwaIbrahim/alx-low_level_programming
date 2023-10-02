32640f9662fe01dc7ee5fdecf1ac18152098637e
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-02 10:34:23 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: count the numbers of the arguments
+ * @argv: the value of the arguments
+ * Return: added numbers
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, sum = 0;
+
+	while (i < argc)
+	{
+		if (*argv[i] >= 48 && *argv[i] <= 57)
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
