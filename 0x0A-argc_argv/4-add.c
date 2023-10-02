3a17085d04c714cbfed321cbb3d30356576bd19c
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-02 11:48:40 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,23 @@ int main(int argc, char *argv[])
 {
 	int i = 1, sum = 0;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 	while (i < argc)
 	{
 		if (*argv[i] >= 48 && *argv[i] <= 57)
-			sum += (atoi(argv[i]));
+		{
+			sum += atoi(argv[i]);
+		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
 		i++;
-	}
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
 	}
 	printf("%d\n", sum);
 	return (0);
