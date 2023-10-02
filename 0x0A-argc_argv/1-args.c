f50d3085e1c9e76e7572c389497c56d4dabb003c
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-02 09:05:56 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives
+ * main - prints the number of arguments passed into it
  * @argc: count the numbers
  * @argv: argument vector
  * Return: the number of arguments
