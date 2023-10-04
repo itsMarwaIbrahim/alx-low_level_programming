4fa07e6a6af04eb2438b16cf7a2dfa2d3007c3a3
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-04 06:56:46 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,10 @@ int **alloc_grid(int width, int height)
 		if (*(ptr + i) == NULL)
 		{
 			for (k = 0; k < i; k++)
+			{
 				free(*(ptr + k));
+				*(ptr + k) = NULL;
+			}
 			free(ptr);
 			return (NULL);
 		}
