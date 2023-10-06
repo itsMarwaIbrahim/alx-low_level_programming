53d936c1f6d6183ff93206a0cd21474c2658f1ae
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-06 09:13:20 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,11 +3,11 @@
 
 /**
  * _calloc - a function to allocates memory for an array
- * @nmemb:
- * @size:
+ * @nmemb: array
+ * @size: bytes
  * Return: a pointer to the allocated memory
- * 	If nmemb or size is 0, then _calloc returns NULL
- * 	If malloc fails, then _calloc returns NULL
+ *	If nmemb or size is 0, then _calloc returns NULL
+ *	If malloc fails, then _calloc returns NULL
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -17,18 +17,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	ptr_calloc = (unsigned int *)malloc(nmemb * size);
-
-	if (ptr_calloc == NULL)
+	if (ptr_calloc == NULL) /* if it fails to allocate, return NULL */
 		return (NULL);
+
 	if (ptr_calloc != NULL)
 	{
-		while (index < size)
+		while (index < size) /* this loop replacing memset function */
 		{
 			ptr_calloc[index] = 0;
 			index++;
 		}
 	}
-	if (ptr_calloc == NULL)
-		return (NULL);
+	free(ptr_calloc);
+
 	return (ptr_calloc);
 }
