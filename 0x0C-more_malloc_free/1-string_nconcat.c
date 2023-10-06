d7331d00acba92a2a4ef2130e4debe14ea75a5b4
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-06 12:03:33 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,52 +11,39 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, i = 0;
-	char *concat, *ptr_s2 = s2; 
+	unsigned int len1 = 0, len2 = 0, i = 0, full_string;
+	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (*s2 != '\0')
+	while (*s2 != '\0' && len2 < n)
 	{
 		s2++;
 		len2++;
 	}
+	s2 -= len2;
 	while (*s1 != '\0')
 	{
 		s1++;
 		len1++;
 	}
+	s1 -= len1;
+
 	if (n >= len2)
 		n = len2;
 
-	concat = (char *)malloc(n + len1 + 1);
+	full_string = len1 + n;
+
+	concat = (char *)malloc(full_string + 1);
 	if (concat == NULL)
 		return (NULL);
 
-	while (i < len1 && j < n)
-	{
-		if (i < len2)
-			concat[ = *s1;
-		else
-			*concat = s2[i - len1];
-		i++;
-	}
-
-	while (*ptr_s2 != '\0')
-	{
-		*s1 = *ptr_s2;
-		s1++;
-		ptr_s2++;
-	}
-	*s1 = '\0';
-
-	if (ptr_s1 == NULL)
-		return (NULL);
-
-	
+	for (i = 0; i < full_string; i++)
+		concat[i] = (i < len1) ? *s1++ : *s2++;
+	concat[full_string] = '\0';
 
 	return (concat);
 }
