f969858561f39921e3dcfe28d701e40d2aeb89ed
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-07 03:43:52 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,26 +4,28 @@
  * times_table - 9 times table
  * @x: first
  * @y: second
- * @z: third
  * Return: nothing
  */
 void times_table(void)
 {
-	int x, y, z;
+	int x = 0, y;
 
-	for (x = 0; x <= 9; x++)
+	while (x <= 9)
 	{
 		_putchar(48);
 		for (y = 1; y <= 9; y++)
 		{
 			_putchar(44);
 			_putchar(32);
-			z = x * y;
-			if (z <= 9)
+
+			if (x * y <= 9)
 				_putchar(32);
 			else
-				_putchar
+				_putchar(x * y / 10 + 48);
+			_putchar (x * y % 10 + 48);
+
 		}
+		x++;
 		_putchar('\n');
 	}
 }
