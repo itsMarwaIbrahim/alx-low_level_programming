3c2fe0f72e907c45e9699c6480b6fbbfe4f499f9
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-07 01:01:05 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *ptr_calloc, *container;
+	unsigned int *ptr_calloc;
+	char *container;
 	unsigned int index = 0;
 
 	if (nmemb == 0 || size == 0)
