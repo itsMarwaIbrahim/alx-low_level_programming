c2441da2c22631be4e623f9a38cf307fff194418
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-07 11:50:18 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,20 +8,20 @@
 char *cap_string(char *string)
 {       
 	char symbols[13] = {']', '[', ')', '(', '\"', '?', '!', '.', ';', ',', '\n', '\t', ' '};
-	int iteration = 0;
+	int iteration = 0, i = 0;
 
 	while (*string != '\0')
 	{
-		if (*string == '\0' && *string == ' ') /* this section represent the toupper function */
+		if (*string == '.' && *string == ' ') /* this section represent the toupper function */
 		{
 			if (*(string + 1) >= 97 && *(string + 1) <= 122)
-			*(string + 1) -= 32;
+				*(string + 1) -= 32;
 		}
 
 		for (iteration = 0; iteration < 13; iteration++)	
 		{
-			if (*string == symbols[iteration] && *(string + 1) >= 97 && *(string + 1) <= 122)
-				*(string + 1) = *(string + 1) - 32;
+			if (string[i] == symbols[iteration] && string[i + 1] >= 97 && string[i + 1] <= 122)
+				string[i + 1] = string[i + 1] - 32;
 		}
 		string++;
 	}
