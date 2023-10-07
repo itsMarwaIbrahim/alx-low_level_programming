7805ae1c23749edb72c438184df1e4e73340ffb1
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-07 11:19:11 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,29 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * @
- * Return:
+ * @string: a string
+ * Return: the pointer to the string
  */
-char *cap_string(char *)
-{
+char *cap_string(char *string)
+{       
+	char symbols[13] = {']', '[', ')', '(', '\"', '?', '!', '.', ';', ',', '\n', '\t', ' '};
+	int iteration = 0;
 
+	while (*string != '\0')
+	{
+		if (*string == '\0' && *string == ' ') /* this section represent the toupper function */
+		{
+			if (*(string + 1) >= 97 && *(string + 1) <= 122)
+			*(string + 1) -= 32;
+		}
+
+		for (iteration = 0; iteration < 13; iteration++)	
+		{
+			if (*string == symbols[iteration] && *(string + 1) >= 97 && *(string + 1) <= 122)
+				*(string + 1) = *(string + 1) - 32;
+		}
+		string++;
+	}
+
+	return (string);
+}
