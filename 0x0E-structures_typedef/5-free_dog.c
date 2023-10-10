2f431937253322bf7b40b2d7c6748b0776ea61ed
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-10 15:29:44 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs (Yep, literally)
+ * @d:
+ * Return: nothing - void
+ */
+void free_dog(dog_t *d)
+{
+
