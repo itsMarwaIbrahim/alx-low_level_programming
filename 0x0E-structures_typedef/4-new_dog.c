4b4d9b8a01451589c6bf0465e32330b36e5e1ade
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-10 11:39:54 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - creates: a (new dog)
+ * @name: the dog's name - a string
+ * @age: the dog's age - a float number
+ * @owner: the dog't owner - a string
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
