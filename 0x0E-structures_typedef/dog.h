8c33bf858b4cc02171fea5d814cc7f5966c3d614
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-10 09:30:38 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * struct dog - structure have a different elemtents
+ * @name: a string - the dog's name
+ * @age: a float number - the dog's age
+ * @owner: a string - the dog owner's name
+ */
 struct dog
 {
 	char *name;
