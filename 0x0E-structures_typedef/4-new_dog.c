c7630801893250e8da2fac88ae51d16492e90d2f
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-10 12:02:03 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,6 +7,29 @@
  * @age: the dog's age - a float number
  * @owner: the dog't owner - a string
  */
+
+int strlen_(char *string)
+{
+	int length = 0;
+
+	while (*string[length])
+		length++;
+}
+
+char *strcpy_(char *destination, char *source)
+{
+	int i = 0;
+
+	while (source[i] != '\0')
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = '\0';
+	return (destination);
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
 
