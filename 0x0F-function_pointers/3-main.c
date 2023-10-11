20b35036555370cea04f0337dc673f81eb1a1940
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-11 14:56:48 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,6 +20,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	opera = (argv[2]);
 
 	if (get_op_func(opera) == NULL)
@@ -27,9 +29,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+	
+	if ((strcmp(opera, "%") == 0 || strcmp(opera, "/") == 0) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	printf("%d\n", get_op_func(opera)(num1, num2));
 	return (0);
 }
