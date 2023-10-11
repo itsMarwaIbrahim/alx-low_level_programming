9dd7c8c9b9a4d27babd5bd351bb562d4d1c112fd
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-11 05:39:48 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name -  prints: a (name)
@@ -8,5 +9,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
+	else
+		return;
 }
