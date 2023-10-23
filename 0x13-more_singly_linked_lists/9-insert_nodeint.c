de0739a07c511eea287da2bb5aff2ae604d2b9f2
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-23 08:52:34 -0700

    9-insert_nodeint.c solved!

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,20 +9,34 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new;
-	unsigned counter = 0;
+	listint_t *new, *ptrHead;
+	unsigned int counter = 0;
 
 	new = (listint_t *)malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 	(*new).n = n;
 
-	while (counter < (idx - 1))
+	if (idx)
 	{
-
-		counter++;
+		ptrHead = *head;
+		while (counter < (idx - 1))
+		{
+			if (!ptrHead)
+			{
+				free(new);
+				return (NULL);
+			}
+			ptrHead = (*ptrHead).next;
+			counter++;
+		}
+		(*new).next = (*ptrHead).next;
+		(*ptrHead).next = new;
 	}
-	if (!idx)
+	else
 	{
 		(*new).next = *head;
-
+		*head = new;
+	}
+	return (new);
+}
