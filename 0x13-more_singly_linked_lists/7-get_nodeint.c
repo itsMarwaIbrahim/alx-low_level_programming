a74ca20996d14798c93d6b76fbb8592f96ffe7f6
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-23 05:20:31 -0700

    Create and solve 7-get_nodeint.c file and task

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of
+ *		a listint_t linked list
+ * @head: a pointer to the head of the linked listint_t list
+ * @index: represents the index of the node to retrieve
+ * Return: returns the nth node of a listint_t linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+
+	if (!head)
+		return (NULL);
+
+	while (counter < index)
+	{
+		head = (*head).next;
+		counter++;
+	}
+
+	return (head);
+}
