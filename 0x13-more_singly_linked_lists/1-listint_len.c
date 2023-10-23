6046dac124f397a88552730ab8770fccde6637b6
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-23 02:58:29 -0700

    Edit some files

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,11 +3,12 @@
 /**
  * listint_len - the number of elements in a linked listint_t list
  * @h: a pointer to the head of linked listint_t list
- * Return:
+ * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t elemCount;
+
 	for (elemCount = 0; h; elemCount++, h = (*h).next)
 		;
 	return (elemCount);
