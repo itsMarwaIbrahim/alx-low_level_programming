a71d690921e0fd4a51967d51092fdecc9a60800b
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-23 11:48:51 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	litstint_t storeTemp;
 	unsigned int counter = 0;
 
-	if (!*head)
+	if (!head)
 		return (NULL);
 
 	for (storeTemp = head; storeTemp; counter++);
