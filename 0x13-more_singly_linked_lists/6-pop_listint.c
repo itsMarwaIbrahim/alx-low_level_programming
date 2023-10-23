4492a6cb9c21a195814cb8c4395f3cfad9e62f53
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-23 04:54:11 -0700

    Create and solve 6-pop_listint.c file and task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list,
+ *		and returns the head node’s data (n)
+ * @head: stores the address of another pointer
+ *		to modify the head of the linked listint_t list
+ * Return: the previous head node value of n member
+ */
+int pop_listint(listint_t **head)
+{
+	int storeData;
+	listint_t *storeTemp;
+
+	if (!head)
+		return (0);
+
+	storeData = (*head)->n;
+	storeTemp = *head;
+	*head = (*head)->next;
+	free(storeTemp);
+
+	return (storeData);
+}
