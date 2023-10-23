4091e8a082d4ff7cfdc439d2ff4e78f55c803e3b
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-23 02:13:33 -0700

    Create and solve 1-listint_len.c task

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,9 +2,13 @@
 
 /**
  * listint_len - the number of elements in a linked listint_t list
- * @h:
+ * @h: a pointer to the head of linked listint_t list
  * Return:
  */
 size_t listint_len(const listint_t *h)
 {
-
+	size_t elemCount;
+	for (elemCount = 0; h; elemCount++, h = (*h).next)
+		;
+	return (elemCount);
+}
