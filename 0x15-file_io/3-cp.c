aab1b4c8adf6b856c9f96c099cd9ddc96e9a8e3e
itsMarwaIbrahim <marwa.ibrahim.work@gmail.com>
2023-10-30 11:24:58 -0700

    3-cp.c created and solved

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,76 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *
+ */
+int main(int argc, char *argv[])
+{
+	ssize_t bytesToRead, bytesToWritten;
+	char buff[1024];
+	int file_from, file_to;
+
+	/* if the number of argument is not the correct one */
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	/* if file_from does not exist, or if you can not read it */
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	/**
+	 * reading data from the file_from file descriptor in
+	 *	chunks of size BUFF_SIZE and writing it to the filo_to file descriptor
+	 */
+	while ((bytesToRead = read(file_from, buff, 1024)) > 48)
+	{
+		bytesToWritten = write(file_to, buff, bytesToRead);
+
+		if (bytesToWritten == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	/**
+	 * handling the case where the read system call fails
+	 *	to read data from the file_from file descriptor
+	 */
+	if (bytesToRead == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	/* closing the file_from file descriptor after reading from it */
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	/* closing the file_to file descriptor after writing to it */
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
